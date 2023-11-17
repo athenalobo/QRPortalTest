b2a69e41e0677420267e75fb9d716bb237aeeedb
MSU <m.stura@castsoftware.com>
2023-11-17 15:57:50 +0100

    TECHNOPLAT-239: enforce https

--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ const bodyParser = require('body-parser');
 const notFound = require('./middleware/notFound');
 const errHandler = require('./middleware/errorHandler');
 const ValidateQuery = require('./rules/lib/validateQuery');
+const enforce = require('express-sslify');
 
 let app = express();
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -29,7 +30,7 @@ app.get('/googleda2a1d9b51c2edfd.html', (req, res) => {
 });
 
 // -------------------------------- Fav ICON -------------------------------------- //
-app.get('/favicon.ico', (req, res)=> {
+app.get('/favicon.ico', (req, res) => {
   //if (req.url.includes('rules')) {
   //res.sendFile(path.resolve(__dirname, '..', 'qrp_WebApp', 'favicon', 'favicon32.png'));
   res.sendFile(path.resolve(__dirname, '..', 'public', 'favicon', 'LogoStructuralRules.png'));
@@ -37,19 +38,19 @@ app.get('/favicon.ico', (req, res)=> {
 
 // ------------------------------- Base redirect ---------------------------------- //
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.redirect('/rules');
 });
 
 app.get('/Echo*', (req, res) => {
   const url = req.originalUrl.replace('/Echo/', '/AIP/');
-  res.redirect( url );
+  res.redirect(url);
 });
 
 app.get('/bestpractices', (req, res) => {
   const queryValid = ValidateQuery(req.query);
-  if(queryValid) {
-    res.sendFile(path.resolve(__dirname, '..', 'public','index.html'), (err) => errHandler(err, res));
+  if (queryValid) {
+    res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'), (err) => errHandler(err, res));
   } else {
     notFound(req, res);
   }
@@ -58,6 +59,6 @@ app.get('/bestpractices', (req, res) => {
 app.use(require('./middleware/notFound'));
 
 // --------------------------- Start Server ---------------------------//
-app.listen(port, function() {
-  console.log('Listening...' + port);	
+app.listen(port, function () {
+  console.log('Listening...' + port);
 });
